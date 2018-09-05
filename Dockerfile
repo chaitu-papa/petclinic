ac5c1030349e465d9725aed8f916351d4489850d
Krishna Papa <chaitu.papa@gmail.com>
2018-09-05 04:18:32 -0400

    Fix

--- Dockerfile
+++ Dockerfile
@@ -26,4 +26,5 @@ RUN mkdir -p ${MACHINE_AGENT_HOME} && \
 COPY tomcat-users.xml /usr/local/tomcat/conf/
 COPY start-service.sh /usr/local/tomcat/bin/start-service.sh
 RUN wget -O /usr/local/tomcat/webapps/spring-petclinic.war http://34.196.120.121:8081/nexus/service/local/artifact/maven/redirect?r=snapshots\&g=org.springframework.samples\&a=spring-petclinic\&v=1.0-SNAPSHOT\&p=war
-CMD "/usr/local/tomcat/bin/start-service.sh"
+WORKDIR /usr/local/tomcat/bin/
+#CMD "start-service.sh"
--- start-service.sh
+++ start-service.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 # Start the first process
-./usr/local/tomcat/bin/catlina.sh start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Tomcat Serive: $status"
-  exit $status
-fi
+"/usr/local/tomcat/bin/catalina.sh start" 
 
 # Start the second process
-./opt/appdynamics/machine-agent/bin/machine-agent &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start machine-agent: $status"
-  exit $status
-fi
+"/opt/appdynamics/machine-agent/bin/machine-agent &"
 
